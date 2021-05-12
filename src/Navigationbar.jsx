baea531eb3b5f721a07d7188956f477c8da72591
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-05-12 12:53:35 +0200

    setup navbar

--- src/Navigationbar.jsx
+++ src/Navigationbar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import Logo from "./images/Logo.svg";
+import './Navigationbar.css'
+import Logo from './images/Logo.svg'
 
 const Navigationbar = () => {
     return(
-        <div>
-            <img src={Logo} />
-            <button>INSERT LOGO</button>
-            <button>Museum</button>
-            <button>Utvalg</button>
-            <button>Om oss</button>
-            <button>Kontakt oss</button>
-            <button>INSERT handlekurv</button>
-            <button>INSERT Login</button>
+        <div className={"navbar-container"}>
+            <button className={'nav-btn'} id={'home-btn'}>
+                <img id={'logo-img'} src={Logo}/>
+            </button>
+            <div className={'container1'}>
+                <button className={'nav-btn'} id={'museum-btn'}>Museum</button>
+                <button className={'nav-btn'} id={'utvalg-btn'}>Utvalg</button>
+                <button className={'nav-btn'} id={'om-oss-btn'}>Om oss</button>
+                <button className={'nav-btn'} id={'kontakt-oss-btn'}>Kontakt oss</button>
+            </div>
+            <div className={'container2'}>
+                <button className={'nav-btn'} id={'cart-btn'}>INSERT handlekurv</button>
+                <button className={'nav-btn'} id={'login-btn'}>INSERT Login</button>
+            </div>
         </div>
     )
 }
