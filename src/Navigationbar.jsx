7706f39d59a30cdcc8c59613652d37ccef85b980
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-05-12 12:00:27 +0200

    created files for Navigation top bar

---
+++ src/Navigationbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navigationbar = () => {
+    return(
+        <div>
+            <button>INSERT LOGO</button>
+            <button>Museum</button>
+            <button>Utvalg</button>
+            <button>Om oss</button>
+            <button>Kontakt oss</button>
+            <button>INSERT handlekurv</button>
+            <button>INSERT Login</button>
+        </div>
+    )
+}
