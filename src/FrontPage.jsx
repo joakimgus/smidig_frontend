9c061afce4396b223b0eab45ef756230d78c82ed
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-05-12 16:19:31 +0200

    picture carusel coming to life. still needs correct styling

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -1,31 +1,51 @@
 import Footer from "./Footer";
 import Navigationbar from "./Navigationbar";
+import './FrontPage.css';
+import Abstract from "./images/Abstract font blub.png";
+import TargetAudience from "./images/Group 155.png";
+import ItemImg from "./images/Image 2.png";
+import Process1 from "./images/kjøpsprosess.png";
+import Process2 from "./images/kjøpsprosess2.png";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import React, {useState} from "react";
 
 const FrontPage = () => {
+    const slideImages = [
+        Abstract,
+        ItemImg,
+        TargetAudience,
+        Process1,
+        Process2
+    ];
+
+    const [autoplay, setAutoplay] = useState(true);
+
   return (
     <div>
       <Navigationbar />
-      <div>Insert bilde-thingy her</div>
+        <div>
+            <div>
+                <Slide autoplay={autoplay}>
+                    <img className={'slide'} src={Abstract} />
+                    <img className={'slide'} src={ItemImg} />
+                    <img className={'slide'} src={TargetAudience} />
+                    <img className={'slide'} src={Process1} />
+                    <img className={'slide'} src={Process2} />
+                </Slide>
+            </div>
+
+            <div className="autoplay-buttons">
+                Autoplay is {autoplay ? 'on' : 'off'}
+            </div>
+            <div className="autoplay-buttons">
+                <button type="button" onClick={() => setAutoplay(false)}>Pause</button>
+                <button type="button" onClick={() => setAutoplay(true)}>Play</button>
+            </div>
+        </div>
       <Footer />
     </div>
   );
 };
-
-
-// Automatic Slideshow - change image every 3 seconds
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000);
-}
 
 export default FrontPage;
