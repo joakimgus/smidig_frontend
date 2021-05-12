4d49c1a4f23de4af4a118063be669e16f95f1385
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-05-12 15:06:04 +0200

    adjusting media queries

--- src/Navigationbar.jsx
+++ src/Navigationbar.jsx
@@ -14,42 +14,38 @@ const Navigationbar = () => {
           <img id={"logo-img"} src={Logo} />
         </button>
       </Link>
-      <div className={"container1"}>
-        <Link to="/museum">
-          <button className={"nav-btn"} id={"museum-btn"}>
-            Museum
-          </button>
-        </Link>
-        <Link to="/utvalg">
-          <button className={"nav-btn"} id={"utvalg-btn"}>
-            Utvalg
-          </button>
-        </Link>
-        <Link to="/om">
-          <button className={"nav-btn"} id={"om-oss-btn"}>
-            Om oss
-          </button>
-        </Link>
-        <Link to="/kontakt">
-          <button className={"nav-btn"} id={"kontakt-oss-btn"}>
-            Kontakt oss
-          </button>
-        </Link>
-      </div>
-      <div className={"container2"}>
-        <Link to="/handlekurv">
-          <button className={"nav-btn"} id={"cart-btn"}>
-            <img src={CartIcon} alt={"Handlekurv ikon"} />
-            <p>Handlekurv</p>
-          </button>
-        </Link>
-        <Link to="/logginn">
-          <button className={"nav-btn"} id={"login-btn"}>
-            <img src={UserIcon} alt={"Bruker ikon"} />
-            <p>Logg inn</p>
-          </button>
-        </Link>
-      </div>
+      <Link to="/museum">
+        <button className={"nav-btn"} id={"museum-btn"}>
+          Museum
+        </button>
+      </Link>
+      <Link to="/utvalg">
+        <button className={"nav-btn"} id={"utvalg-btn"}>
+          Utvalg
+        </button>
+      </Link>
+      <Link to="/om">
+        <button className={"nav-btn"} id={"om-oss-btn"}>
+          Om oss
+        </button>
+      </Link>
+      <Link to="/kontakt">
+        <button className={"nav-btn"} id={"kontakt-oss-btn"}>
+          Kontakt oss
+        </button>
+      </Link>
+      <Link to="/handlekurv">
+        <button className={"nav-btn"} id={"cart-btn"}>
+          <img src={CartIcon} alt={"Handlekurv ikon"} />
+          <p>Handlekurv</p>
+        </button>
+      </Link>
+      <Link to="/logginn">
+        <button className={"nav-btn"} id={"login-btn"}>
+          <img src={UserIcon} alt={"Bruker ikon"} />
+          <p>Logg inn</p>
+        </button>
+      </Link>
     </div>
   );
 };
