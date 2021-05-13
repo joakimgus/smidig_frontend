3a8dd945ec27f0f6989fc5229f447ed7c1aa5c9a
joakimgus <55795916+joakimgus@users.noreply.github.com>
2021-05-13 16:09:03 +0200

    footer part 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,15 +19,22 @@ const Footer = () => {
       </div>
       <div className={"contact-information-container"}>
         <div className={"contact-information-item"}>KONTAKT OSS</div>
-        <div className={"contact-information-item"}>mobiltminimuseum.no</div>
-        <div className={"contact-information-item"}>Adresse</div>
-        <div className={"contact-information-item"}>0123 Fire</div>
-        <div className={"contact-information-item"}>Tlf 11 22 33 44</div>
-        <div className={"contact-information-item"}>
+        <div className={"contact-information-item contact-info"}>mobiltminimuseum.no</div>
+        <div className={"contact-information-item contact-info"}>Adresse</div>
+        <div className={"contact-information-item contact-info"}>0123 Fire</div>
+        <div className={"contact-information-item contact-info"}>Tlf 11 22 33 44</div>
+        <div className={"contact-information-item contact-info"}>
           info@mobiltminimuseum.no
         </div>
       </div>
-      <div className={"three"}>three</div>
+      <div className={"about-us-container"}>
+          <div className={"about-us-title"}>
+              mobiltminumuseum.no
+          </div>
+          <div className={"about-us-information"}>
+              Mobiltminimuseum.no distribuerer materiell fra museum og lignende kulturorganisasjoner til barnehager, eldrehjem og lignende institusjoner i Norge. Tjenesten er gratis å benytte for lærere og helsepersonell ved godkjente institusjoner.
+          </div>
+      </div>
       <div className={"four"}>four</div>
     </div>
   );
