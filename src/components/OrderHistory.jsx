a7bf681da720f9690370e92ed2d834bb670b41f5
joakimgus <55795916+joakimgus@users.noreply.github.com>
2021-05-20 12:56:46 +0200

    table formatting

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -8,11 +8,18 @@ const OrderHistory = () => {
                 <p>En oversikt over tidligere ordre du har gjennomført.</p>
                 <table id={"order-history-table"}>
                     <tr>
-                        <th>Dato</th>
-                        <th>Ordrenummer</th>
-                        <th>Musuem</th>
-                        <th>Pakke</th>
-                        <th>Pris</th>
+                        <th className={"table-header"}>Dato</th>
+                        <th className={"table-header"}>Ordrenummer</th>
+                        <th className={"table-header"}>Museum</th>
+                        <th className={"table-header"}>Pakke</th>
+                        <th className={"table-header"}>Pris</th>
+                    </tr>
+                    <tr>
+                        <td>01.05.2021</td>
+                        <td>120398120</td>
+                        <td>Tidvis</td>
+                        <td>Oslo 1324</td>
+                        <td>0.-</td>
                     </tr>
                 </table>
             </div>
