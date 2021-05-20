6213f8c4796158ffb666d737c3bea768a755afac
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-05-20 14:24:54 +0200

    setup cart, not done yet.

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './Cart.css';
+
+const Cart = () => {
+    const imgPaaaath = require('../images/vikingsskiphuset.png').default;
+    return(
+        <div className={'cart-page-container'}>
+            <div className={'cart-top-text-container'}>
+                <h1>Handlekurv</h1>
+                <hr />
+                <p>Her vil du finne en oversikt over hvilke varer du har i handlekurven din.</p>
+            </div>
+            <div className={'cart-table-specifications'}>
+                <h5 className={'product-title-section'}>Produkt</h5>
+                <h5 className={'amount-title-section'}>Antall</h5>
+                <h5 className={'price-title-section'}>Pris</h5>
+                <h5 className={'remove-title-section'}>Slett</h5>
+                <hr />
+            </div>
+            <div className={'cart-item-container'}>
+                <img src={imgPaaaath} alt={'item image'} />
+                <div className={'cart-item-text-container'}>
+                    <h3>Produktnavn</h3>
+                    <p>Produktbeskrivelse</p>
+                    <p>Artikkelnr: 098765</p>
+                </div>
+                <div className={'item-amount-container'}>
+                    {/*TODO Komponent her? //Counter*/}
+                    <p><span>-</span> 1 <span>+</span></p>
+                </div>
+                <div className={'item-price-container'}>
+                    <p>NOK 0.00</p>
+                </div>
+                <div className={'item-remove-container'}>
+                    <button>x</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cart;
