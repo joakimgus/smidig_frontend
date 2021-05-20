971b4eaf9b5178ab2a9bb2e5bc70f923de47d91c
magnuen2k <maen030@student.kristiania.no>
2021-05-20 14:51:38 +0200

    add products to localstorage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -5,9 +5,17 @@ const ProductPage = () => {
   const location = useLocation();
   const data = location.state.params;
 
+  const addToCart = () => {
+    console.log("Legg til ");
+    let cart = JSON.parse(localStorage.getItem("handlekurv")) || [];
+    cart.push(data);
+    localStorage.setItem("handlekurv", JSON.stringify(cart));
+  };
+
   return (
     <div>
       <p>{data.name}</p>
+      <button onClick={addToCart}>Legg til i handlekurv</button>
     </div>
   );
 };
