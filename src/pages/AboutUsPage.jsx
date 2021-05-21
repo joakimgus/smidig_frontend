67c6d33288f60b5098dca170eb96ed804e0a26dd
smeepz <stianlw@gmail.com>
2021-05-21 11:58:31 +0200

    about us page basic info added

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const AboutUsPage = () => {
   return (
     <div>
-      <div>tidvis er et selskap som gjør ting og sånt</div>
+      <div>
+          <h1>Tidvis</h1>
+          <p>
+              Tidvis er et ideelt selskap som utforsker og utvikler nye måter å formidle og tilgjengeliggjøre forskning på, med et spesielt fokus på historie.
+              Vi mener historie er viktig, fordi det danner utgangspunktet for vår forståelse av nåtiden og hva vi forventer av fremtiden.
+              Vår målsetning er å benytte både velkjente og nye virkemidler og teknologier. Eksempler på slike er databaser, spill, 3D-modeller,
+              augmented og virtual reality, animasjoner, interaktive sanseopplevelser og god, fleksibel design på alle flater.
+              Vi ønsker å nå bredt ut til flest mulig med elegante løsninger.
+              Slik gjør vi det lettere for mennesker i dag å få erfaring med fortiden, og legger til rette for at flere får et bredere kunnskapsgrunnlag for å reflektere over nåtiden og fremtiden.
+          </p>
+      </div>
     </div>
   );
 };
