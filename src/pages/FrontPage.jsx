dff120da771043f46c1feee92ef249dc605e465b
magnuen2k <maen030@student.kristiania.no>
2021-05-23 14:50:17 +0200

    autoswipe possibility (commented out for now)

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
 import "./FrontPage.css";
 import SlideOne from "../components/SlideOne";
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SlideTwo from "../components/SlideTwo";
 import SlideThree from "../components/SlideThree";
+import SlideFour from "../components/SlideFour";
+import SlideFive from "../components/SlideFive";
 
 // Import Swiper styles
 import "swiper/swiper.scss";
 import "swiper/swiper-bundle.css";
-import SlideFour from "../components/SlideFour";
-import SlideFive from "../components/SlideFive";
 
 const FrontPage = () => {
   // Install modules
-  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
   // const slideImages = [
   //     Abstract,
   //     ItemImg,
@@ -26,6 +32,10 @@ const FrontPage = () => {
   return (
     <div className={"page-container"}>
       <Swiper
+        /*autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}*/
         spaceBetween={0}
         slidesPerView={1}
         pagination={{ clickable: true }}
