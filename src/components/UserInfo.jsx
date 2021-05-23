33529d55f7b9896247391121b364dbaa5e555745
magnuen2k <maen030@student.kristiania.no>
2021-05-23 20:24:12 +0200

    fix logout and minside

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,58 +1,72 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
-import {UserContext} from "../context/context";
+import { UserContext } from "../context/context";
 import "./UserInfo.css";
 import Edit from "../images/edit.svg";
-import {fetchData} from "../api/apiHandler";
+import { fetchData } from "../api/apiHandler";
 
 const UserInfo = () => {
-    const { user, setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const logout = async () => {
-        const res = await fetchData("/auth/logout");
-        console.log(res);
-        if (res === "done") {
-            setUser(undefined);
-            history.push("/");
-        }
+  const logout = async () => {
+    const res = await fetchData("/auth/logout");
+    console.log(res);
+    if (res === "done") {
+      setUser(undefined);
+      history.push("/");
     }
+  };
 
-    console.log(user)
+  console.log(user);
 
-    return(
-        <>
-            <div className={"user-info-container"}>
-                <div id={"user-info-text-container"}>
-                    <h1 id={"user-info-title"}>Min side</h1>
-                    <p>Du er logget inn som bruker: </p>
-                </div>
-                <table id={"user-info-table"}>
-                    <tr className={"user-row"}>
-                        <td className={"user-info-text"}>E-post: {user.email}</td>
-                        <td><button className={"edit-btn"}><img src={Edit} /></button></td>
-                    </tr>
-                    <tr className={"user-row"}>
-                        <td className={"user-info-text"}>Org. nr.: {user.orgNr}</td>
-                        <td><button className={"edit-btn"}><img src={Edit} /></button></td>
-                    </tr>
-                    <tr className={"user-row"}>
-                        <td className={"user-info-text"}>Password: ********</td>
-                        <td className={"edit-btn-column"}><button className={"edit-btn"}><img src={Edit} /></button></td>
-                    </tr>
-                </table>
-                <Link onClick={logout} id={"log-out-btn"}>
-                    <button>
-                        <p>Logg ut</p>
-                    </button>
-                </Link>
-                <div id={"tidvis-logo"}>
-                    <img src={require("../images/tidvis-rund.png").default} />
-                </div>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      {user && (
+        <div className={"user-info-container"}>
+          <div id={"user-info-text-container"}>
+            <h1 id={"user-info-title"}>Min side</h1>
+            <p>Du er logget inn som bruker: </p>
+          </div>
+          <table id={"user-info-table"}>
+            <tr className={"user-row"}>
+              <td className={"user-info-text"}>E-post: {user.email}</td>
+              <td>
+                <button className={"edit-btn"}>
+                  <img src={Edit} />
+                </button>
+              </td>
+            </tr>
+            <tr className={"user-row"}>
+              <td className={"user-info-text"}>Org. nr.: {user.orgNr}</td>
+              <td>
+                <button className={"edit-btn"}>
+                  <img src={Edit} />
+                </button>
+              </td>
+            </tr>
+            <tr className={"user-row"}>
+              <td className={"user-info-text"}>Password: ********</td>
+              <td className={"edit-btn-column"}>
+                <button className={"edit-btn"}>
+                  <img src={Edit} />
+                </button>
+              </td>
+            </tr>
+          </table>
+          <Link onClick={logout} id={"log-out-btn"}>
+            <button>
+              <p>Logg ut</p>
+            </button>
+          </Link>
+          <div id={"tidvis-logo"}>
+            <img src={require("../images/tidvis-rund.png").default} />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default UserInfo;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,15 +2,23 @@ import React, { useContext } from "react";
 import UserInfo from "../components/UserInfo";
 import OrderHistory from "../components/OrderHistory";
 import "./ProfilePage.css";
+import { UserContext } from "../context/context";
 
 const ProfilePage = () => {
+  const { user } = useContext(UserContext);
 
-    return(
+  return (
+    <div>
+      {user ? (
         <div className={"profile-page-container"}>
-            <UserInfo />
-            <OrderHistory />
+          <UserInfo />
+          <OrderHistory />
         </div>
-    );
-}
+      ) : (
+        <div>log in</div>
+      )}
+    </div>
+  );
+};
 
 export default ProfilePage;
