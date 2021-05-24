ba7b9f13db6b408763ce8496b619ca2ad23631ac
joakimgus <55795916+joakimgus@users.noreply.github.com>
2021-05-24 19:06:54 +0200

    get user and order info from localstorage

--- src/pages/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation.jsx
@@ -1,7 +1,30 @@
+import React, { useState, useEffect } from "react";
+
 const OrderConfirmation = () => {
+    const [order, setOrder] = useState();
+    const [cart, setCart] = useState();
+
+    useEffect(() => {
+        let savedOrder = JSON.parse(localStorage.getItem("checkout")) || [];
+        let savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+        setCart(savedCart);
+        setOrder(savedOrder);
+    }, []);
+
     return(
-        <>
-        </>
+        <div id={"order-confirmation-container"}>
+            <div id={"order-confirmation-img-container"}>
+                <img
+                    src={require("../images/payment-information.png").default}
+                    alt={"order-confirmation"}
+                />
+            </div>
+            <div id={"order-confirmation-header-text"}>
+                <h1>BEKREFTELSE</h1>
+                <hr />
+                <p>Bestillingen er fullført og du vil få tilsendt en bekreftelse på mail.</p>
+            </div>
+        </div>
     );
 }
 
