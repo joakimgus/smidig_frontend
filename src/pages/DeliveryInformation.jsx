18bdbe4eb7edc0e14d436dbd0092992b7c4b5a6e
joakimgus <55795916+joakimgus@users.noreply.github.com>
2021-05-24 13:54:00 +0200

    delivery information page and fixed the impossible bug of text area resizing on contact page

--- src/pages/DeliveryInformation.jsx
+++ src/pages/DeliveryInformation.jsx
@@ -1,7 +1,42 @@
+import {Input} from "../components/Input";
+import {Link} from "react-router-dom";
+import "./DeliveryInformation.css";
+
 const DeliveryInformation = () => {
     return(
-        <>
-        </>
+        <div id={"delivery-information-container"}>
+            <div id={"delivery-information-img-container"}>
+                <img src={require("../images/delivery-information.png").default}  alt={"delivery-information"}/>
+            </div>
+            <div id={"delivery-information-header-text"}>
+                <h1>LEVERING</h1>
+                <hr />
+                <p>Fyll inn leveringsadresse</p>
+            </div>
+            <div id={"delivery-information-form-container"}>
+                <form>
+                    <Input
+                        label={"ADRESSE"}
+                        type={"text"}
+                        name={"adress"}
+                        handleChange={""}
+                    />
+                    <Input
+                        label={"POSTNUMMER"}
+                        type={"text"}
+                        name={"zip-code"}
+                        handleChange={""}
+                    />
+                    <Input
+                        label={"STED"}
+                        type={"text"}
+                        name={"city"}
+                        handleChange={""}
+                    />
+                    <Link to={"/leveringsinformasjon"}><button id={"confirm-btn"}>Bekreft</button></Link>
+                </form>
+            </div>
+        </div>
     );
 }
 
