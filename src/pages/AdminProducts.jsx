355456b4d15a38fa39fd0601bdd84ef13766b07e
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-05-27 15:11:55 +0200

    added page for AdminProducts and stylesheet

--- src/pages/AdminProducts.jsx
+++ src/pages/AdminProducts.jsx
@@ -1,12 +1,26 @@
+import React from "react";
+import './AdminProducts.css';
+
 const AdminProducts = () => {
     return(
-        <div id={"admin-products-container"}>
-            <h3>test products</h3>
-            <h3>test products</h3>
-            <h3>test products</h3>
-            <h3>test products</h3>
+        <div className={'admin-products-page-container'}>
+            <div className={"admin-top-text-container"}>
+                <h1>Handlekurv</h1>
+                <p>
+                    Her vil du finne en oversikt over hvilke varer du har i handlekurven
+                    din.
+                </p>
+            </div>
+            <div className={"admin-products-table-specifications"}>
+                <h5>Dato lagt til</h5>
+                <h5>Artikkelnr.</h5>
+                <h5>Museum</h5>
+                <h5>Produktnavn</h5>
+                <h5>Antall</h5>
+                <h5>Slett</h5>
+                <hr />
+            </div>
         </div>
-    );
+    )
 }
-
 export default AdminProducts;
