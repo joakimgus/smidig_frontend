71068d8070ba295d07a9af626055565bfa0fc262
magnuen2k <maen030@student.kristiania.no>
2021-05-27 18:15:04 +0200

    render products on admin page

--- src/pages/AdminProducts.jsx
+++ src/pages/AdminProducts.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { fetchData } from "../api/apiHandler";
 import Loading from "../components/Loading";
+import moment from "moment";
+import "./AdminProducts.css";
 
 const AdminProducts = () => {
   const [products, setProducts] = useState();
@@ -11,17 +13,49 @@ const AdminProducts = () => {
     });
   }, []);
 
+  const deleteProduct = async (id) => {
+    console.log(id);
+  };
+
   if (!products) {
     return <Loading />;
   }
 
   return (
     <div id={"admin-products-container"}>
-      {products.map((p) => (
-        <>
-          <p>{p.name}</p>
-        </>
-      ))}
+      <table id={"order-history-table"}>
+        <tr id={"header-row"}>
+          <th className={"table-header date-column"}>Dato</th>
+          <th className={"table-header ordernr-column"}>Artikkel Nr.</th>
+          <th className={"table-header museum-column"}>Museum</th>
+          <th className={"table-header museum-column"}>Produktnavn</th>
+          <th className={"table-header museum-column"}>Antall</th>
+          <th className={"table-header museum-column"}>Slett</th>
+        </tr>
+        {products.map((o) => (
+          <tr className={"order-row"}>
+            <td className={"date-column"}>
+              {moment(o.orderDate).format("YYYY-MM-DD HH:mm")}
+            </td>
+            <td className={"ordernr-column"}>{o._id}</td>
+            <td className={"museum-column"}>
+              <div>{o.developer}</div>
+            </td>
+            <td className={"package-column"}>
+              <div>{o.name}</div>
+            </td>
+            <td className={"package-column"}>
+              <div>{o.stock}</div>
+            </td>
+            <td
+              className={"package-column button"}
+              onClick={() => deleteProduct(o._id)}
+            >
+              X
+            </td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
