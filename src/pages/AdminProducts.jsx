873899738a6281b3a453323c87298bbb7a8115b0
magnuen2k <maen030@student.kristiania.no>
2021-05-27 17:45:49 +0200

    get all products from backend

--- src/pages/AdminProducts.jsx
+++ src/pages/AdminProducts.jsx
@@ -1,12 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../api/apiHandler";
+import Loading from "../components/Loading";
+
 const AdminProducts = () => {
-    return(
-        <div id={"admin-products-container"}>
-            <h3>test products</h3>
-            <h3>test products</h3>
-            <h3>test products</h3>
-            <h3>test products</h3>
-        </div>
-    );
-}
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    fetchData("/products").then((res) => {
+      setProducts(res);
+    });
+  }, []);
+
+  if (!products) {
+    return <Loading />;
+  }
+
+  return (
+    <div id={"admin-products-container"}>
+      {products.map((p) => (
+        <>
+          <p>{p.name}</p>
+        </>
+      ))}
+    </div>
+  );
+};
 
 export default AdminProducts;
