48118ca3e231f45f9c06d404eb5a2427f08258da
Kaikka <kaileeto@gmail.com>
2021-05-28 12:33:11 +0200

    Lists all users and "can" delete them also fix?

--- src/pages/ManageUsers.jsx
+++ src/pages/ManageUsers.jsx
@@ -1,11 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../api/apiHandler";
+import Loading from "../components/Loading";
+
 const ManageUsers = () => {
-    return(
-        <div id={"manage-users-container"}>
-            <h3>user test</h3>
-            <h3>user test</h3>
-            <h3>user test</h3>
-        </div>
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetchData("/auth/getUsers").then((res) => {
+      setUsers(res);
+    });
+  }, []);
+
+  const deleteUser = async (id) => {
+    console.log(
+      "Attempting to delete user with id " +
+        id +
+        ", but won't actually do it for now"
     );
-}
+  };
+
+  if (!users) {
+    return <Loading />;
+  }
+
+  return (
+    <div id={"manage-users-container"}>
+      <table>
+        <tr id={"header-row"}>
+          <th>Email</th>
+        </tr>
+        {users?.map((u) => (
+          <tr>
+            <td>{u.email}</td>
+            <td>{u.type}</td>
+            <td>{u.orgNr}</td>
+            <td onClick={() => deleteUser(u._id)}>Slett bruker</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
 
 export default ManageUsers;
