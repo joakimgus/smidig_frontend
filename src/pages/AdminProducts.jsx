81a9fa61852364a64483baf05bee9db1c731656f
Kaikka <kaileeto@gmail.com>
2021-05-29 23:43:34 +0200

    console.logz

--- src/pages/AdminProducts.jsx
+++ src/pages/AdminProducts.jsx
@@ -3,7 +3,7 @@ import { fetchData } from "../api/apiHandler";
 import Loading from "../components/Loading";
 import moment from "moment";
 import "./AdminProducts.css";
-import {TiDelete} from "react-icons/all";
+import { TiDelete } from "react-icons/all";
 
 const AdminProducts = () => {
   const [products, setProducts] = useState();
@@ -15,7 +15,7 @@ const AdminProducts = () => {
   }, []);
 
   const deleteProduct = async (id) => {
-    console.log(id);
+    console.log("Deleting product " + id + ", but not really");
   };
 
   if (!products) {
@@ -23,24 +23,26 @@ const AdminProducts = () => {
   }
 
   return (
-      <div className={"admin-products-page-container"}>
-        <div className={"admin-products-top-text-container"}>
-          <h1>Produkter</h1>
-          <p>
-            Her vil du finne en oversikt over hvilke enkelt produkter som ligger i databasen. Disse produktene kan brukes til å sette sammen pakkeløsninger.
-          </p>
-        </div>
-        <div className={"admin-table-specifications"}>
-          <h5 className={"date-title-section"}>Dato lagt til</h5>
-          <h5 className={"product-id-title-section"}>Artikkelnr.</h5>
-          <h5 className={"owner-title-section"}>Museum</h5>
-          <h5 className={"product-name-title-section"}>Produktnavn</h5>
-          <h5 className={"product-amount-title-section"}>Antall</h5>
-          <h5 className={"delete-title-section"}>Slett</h5>
-          <hr />
-        </div>
-        {products.map((o) => (
-          <>
+    <div className={"admin-products-page-container"}>
+      <div className={"admin-products-top-text-container"}>
+        <h1>Produkter</h1>
+        <p>
+          Her vil du finne en oversikt over hvilke enkelt produkter som ligger i
+          databasen. Disse produktene kan brukes til å sette sammen
+          pakkeløsninger.
+        </p>
+      </div>
+      <div className={"admin-table-specifications"}>
+        <h5 className={"date-title-section"}>Dato lagt til</h5>
+        <h5 className={"product-id-title-section"}>Artikkelnr.</h5>
+        <h5 className={"owner-title-section"}>Museum</h5>
+        <h5 className={"product-name-title-section"}>Produktnavn</h5>
+        <h5 className={"product-amount-title-section"}>Antall</h5>
+        <h5 className={"delete-title-section"}>Slett</h5>
+        <hr />
+      </div>
+      {products.map((o) => (
+        <>
           <div className="admin-products-container">
             <div className="admin-product-order-date-column">
               <p className="product-date-added">
@@ -48,9 +50,7 @@ const AdminProducts = () => {
               </p>
             </div>
             <div className="admin-product-order-nr-column">
-              <p className={"order-nr"}>
-                {o._id}
-              </p>
+              <p className={"order-nr"}>{o._id}</p>
             </div>
             <div className="admin-product-owner-column">
               <p className={"product-owner-museum"}>{o.developer}</p>
@@ -63,15 +63,15 @@ const AdminProducts = () => {
             </div>
             <div className="admin-product-delete-column">
               <p
-                  className={"product-delete-btn button"}
-                  onClick={() => deleteProduct(o._id)}
+                className={"product-delete-btn button"}
+                onClick={() => deleteProduct(o._id)}
               >
                 <TiDelete />
               </p>
             </div>
           </div>
-          </>
-        ))}
+        </>
+      ))}
     </div>
   );
 };
