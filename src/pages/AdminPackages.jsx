6493764a92ce05d8cbf6a57056783ce43f92cc9b
Kaikka <kaileeto@gmail.com>
2021-05-29 23:44:07 +0200

    baseline for showing packages but there's "some" fix required

--- src/pages/AdminPackages.jsx
+++ src/pages/AdminPackages.jsx
@@ -1,12 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { fetchData } from "../api/apiHandler";
+import Loading from "../components/Loading";
+
 const AdminPackages = () => {
-    return(
-        <div id={"admin-packages-container"}>
-            <h3>Test package</h3>
-            <h3>Test package</h3>
-            <h3>Test package</h3>
-            <h3>Test package</h3>
-        </div>
-    );
-}
+  const [packages, setPackages] = useState();
+
+  useEffect(() => {
+    fetchData("/exhibitions").then((res) => {
+      setPackages(res);
+    });
+  });
+
+  const deletePackage = async (id) => {
+    console.log("Deleting package " + id + ", but not really");
+  };
+
+  if (!packages) {
+    return <Loading />;
+  }
+
+  return (
+    <div id={"admin-packages-container"}>
+      <div className={"admin-packages-top-text-container"}>
+        <h1>Pakker</h1>
+        <p>
+          Jeg kan ikke skrive, dette er slik produkter-siden har ting, bare noe
+          litt mer meningsfullt her (fix)
+        </p>
+      </div>
+      {packages.map((p) => (
+        <>
+          <div>
+            {/*Dette burde linke videre til en egen side for den spesifike pakken der man kan endre på alt + vise alt*/}
+            <p>Navn: {p.name}</p>
+            <p>ID/Artikkelnr: {p._id}</p>
+            <p>ID utvikler(museum): {p.developer}</p>
+            <p>Aktiv: {p.isActive ? "Aktiv" : "Inaktiv"}</p>{" "}
+            {/*Kanskje en knapp her for å endre aktiv:inaktiv?*/}
+            <p></p>
+          </div>
+          <br />
+        </>
+      ))}
+    </div>
+  );
+};
 
 export default AdminPackages;
