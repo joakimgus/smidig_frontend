f5e2302c33d413d663f95f0d677e8ace27172961
Kaikka <kaileeto@gmail.com>
2021-05-31 11:47:47 +0200

    fixed missing deps for useeffect

--- src/pages/AdminPackages.jsx
+++ src/pages/AdminPackages.jsx
@@ -1,4 +1,4 @@
-/*import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchData } from "../api/apiHandler";
 import Loading from "../components/Loading";
 
@@ -9,7 +9,7 @@ const AdminPackages = () => {
     fetchData("/exhibitions").then((res) => {
       setPackages(res);
     });
-  });
+  }, []);
 
   const deletePackage = async (id) => {
     console.log("Deleting package " + id + ", but not really");
@@ -28,32 +28,19 @@ const AdminPackages = () => {
           litt mer meningsfullt her (fix)
         </p>
       </div>
-      {packages.map((p) => (
+      {packages?.map((p) => (
         <>
           <div>
-            {Dette burde linke videre til en egen side for den spesifike pakken der man kan endre på alt + vise alt}
+            {/*Dette burde linke videre til en egen side for den spesifike pakken der man kan endre på alt + vise alt*/}
             <p>Navn: {p.name}</p>
             <p>ID/Artikkelnr: {p._id}</p>
             <p>ID utvikler(museum): {p.developer}</p>
             <p>Aktiv: {p.isActive ? "Aktiv" : "Inaktiv"}</p>{" "}
-            {Kanskje en knapp her for å endre aktiv:inaktiv?}
-            <p></p>
+            {/*Kanskje en knapp her for å endre aktiv:inaktiv?*/}
           </div>
           <br />
         </>
       ))}
-    </div>
-  );
-};
-*/
-
-const AdminPackages = () => {
-  return (
-    <div id={"admin-packages-container"}>
-      <h3>Test package</h3>
-      <h3>Test package</h3>
-      <h3>Test package</h3>
-      <h3>Test package</h3>
     </div>
   );
 };
