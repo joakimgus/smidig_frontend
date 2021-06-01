5b4a1359c2d7b38244cade2a3763017b21ff3d33
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-06-01 14:15:16 +0200

    setup about us page, ready for styling

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import "./style/AboutUsPage.css";
+import LogoMiniMuseum from '../images/hovedlogo-web.png';
+import LogoTidvis from '../images/tidvis-rund.png';
 
 const AboutUsPage = () => {
+
+  const aboutUs = [
+    {
+      name: 'Mobilt minimuseum',
+      description: 'Mobilt minimuseum er datterselskapet til Tidvis. Mobilt minimuseums oppgave er å gi bedre tilgang på museum og kulturliv.',
+      logo: LogoMiniMuseum
+    },
+    {
+      name: 'Tidvis',
+      description: 'Tidvis er et ideelt selskap som utforsker og utvikler nye måter å formidle og tilgjengeliggjøre forskning på, med et spesielt fokus på historie. Vi mener historie er viktig, fordi det danner utgangspunktet for vår forståelse av nåtiden og hva vi forventer av fremtiden. Vår målsetning er å benytte både velkjente og nye virkemidler og teknologier. Eksempler på slike er databaser, spill, 3D-modeller, augmented og virtual reality, animasjoner, interaktive sanseopplevelser og god, fleksibel design på alle flater. Vi ønsker å nå bredt ut til flest mulig med elegante løsninger. Slik gjør vi det lettere for mennesker i dag å få erfaring med fortiden, og legger til rette for at flere får et bredere kunnskapsgrunnlag for å reflektere over nåtiden og fremtiden.',
+      logo: LogoTidvis
+    }
+  ]
+
   return (
     <div className={"about-us-page-container"}>
-      <div>
-        <h1>Tidvis</h1>
-        <p>
-          Tidvis er et ideelt selskap som utforsker og utvikler nye måter å
-          formidle og tilgjengeliggjøre forskning på, med et spesielt fokus på
-          historie. Vi mener historie er viktig, fordi det danner utgangspunktet
-          for vår forståelse av nåtiden og hva vi forventer av fremtiden. Vår
-          målsetning er å benytte både velkjente og nye virkemidler og
-          teknologier. Eksempler på slike er databaser, spill, 3D-modeller,
-          augmented og virtual reality, animasjoner, interaktive
-          sanseopplevelser og god, fleksibel design på alle flater. Vi ønsker å
-          nå bredt ut til flest mulig med elegante løsninger. Slik gjør vi det
-          lettere for mennesker i dag å få erfaring med fortiden, og legger til
-          rette for at flere får et bredere kunnskapsgrunnlag for å reflektere
-          over nåtiden og fremtiden.
-        </p>
-      </div>
+      {aboutUs.map((m, i) => (
+          <div
+              className={'company-wrapper'}
+              key={i}
+          >
+            <h1>{m.name}</h1>
+            <p>{m.description}</p>
+            <img src={m.logo} alt={'logo'} style={{height: '15em', objectFit: 'scale-down'}} />
+          </div>
+      ))}
     </div>
   );
 };
