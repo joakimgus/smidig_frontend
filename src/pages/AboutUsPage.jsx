af80f4edbe66ce44dff7de3e9716dc19594eb532
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-06-02 17:41:35 +0200

    small fix, still with placeholdertext

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -8,18 +8,13 @@ const AboutUsPage = () => {
   const aboutUs = [
     {
       name: 'Hvem er vi?',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus deserunt dicta, esse, eum excepturi explicabo itaque iusto labore nobis officiis, quaerat recusandae reiciendis rem sequi temporibus totam ullam velit voluptas.',
+      description: 'Hvem er tidvis og Mobilt minimuseum (datterselskapet)? Tidvis er et ideelt selskap som utforsker og utvikler nye måter å formidle og tilgjengeliggjøre forskning på, med et spesielt fokus på historie. Vi mener historie er viktig, fordi det danner utgangspunktet for vår forståelse av nåtiden og hva vi forventer av fremtiden. Vår målsetning er å benytte både velkjente og nye virkemidler og teknologier. Eksempler på slike er databaser, spill, 3D-modeller, augmented og virtual reality, animasjoner, interaktive sanseopplevelser og god, fleksibel design på alle flater. Vi ønsker å nå bredt ut til flest mulig med elegante løsninger. Slik gjør vi det lettere for mennesker i dag å få erfaring med fortiden, og legger til rette for at flere får et bredere kunnskapsgrunnlag for å reflektere over nåtiden og fremtiden.',
       logo: LogoTidvis
     },
     {
-      name: 'Hva gjør vi?',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus deserunt dicta, esse, eum excepturi explicabo itaque iusto labore nobis officiis, quaerat recusandae reiciendis rem sequi temporibus totam ullam velit voluptas.',
+      name: 'Vårt formål',
+      description: 'Formålet med mobiltminimuseum ===',
       logo: LogoMiniMuseum
-    },
-    {
-      name: 'Tidvis',
-      description: 'Tidvis er et ideelt selskap som utforsker og utvikler nye måter å formidle og tilgjengeliggjøre forskning på, med et spesielt fokus på historie. Vi mener historie er viktig, fordi det danner utgangspunktet for vår forståelse av nåtiden og hva vi forventer av fremtiden. Vår målsetning er å benytte både velkjente og nye virkemidler og teknologier. Eksempler på slike er databaser, spill, 3D-modeller, augmented og virtual reality, animasjoner, interaktive sanseopplevelser og god, fleksibel design på alle flater. Vi ønsker å nå bredt ut til flest mulig med elegante løsninger. Slik gjør vi det lettere for mennesker i dag å få erfaring med fortiden, og legger til rette for at flere får et bredere kunnskapsgrunnlag for å reflektere over nåtiden og fremtiden.',
-      logo: ''
     }
   ]
 
