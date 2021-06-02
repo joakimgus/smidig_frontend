0f7f2cdc38441f7b73f87527a32227dc5c468218
magnuen2k <maen030@student.kristiania.no>
2021-06-02 15:30:20 +0200

    add initial state for package info

--- src/pages/Superuser/SuperuserAddPackageInfo.jsx
+++ src/pages/Superuser/SuperuserAddPackageInfo.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SuperUserAddPackageInfo = () => {
+  const [info, setInfo] = useState({
+    media: [],
+    tags: [],
+    requiredEquipment: [],
+    name: "",
+    shortDescription: "",
+    description: "",
+  });
+
   return (
     <div style={{ padding: "10rem 2rem" }}>
       <p>halla</p>
